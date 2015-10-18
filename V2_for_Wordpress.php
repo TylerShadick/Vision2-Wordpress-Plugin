6d0a919b5963195a46ee69ecbb61a2a50480ed57
Tyler Shadick <tyler.shadick@gmail.com>
2015-10-17 22:34:38 -0400

    Function Name Mismatch
    add_shortcode function mismatch corrected

--- V2_for_Wordpress.php
+++ V2_for_Wordpress.php
@@ -15,16 +15,15 @@ class Vision2_Shortcode {
 	static $add_v2_script;
 
 	static function init() {
-		add_v2_shortcode('Vision2', array(__CLASS__, 'handle_v2_shortcode'));
+		add_shortcode('Vision2', array(__CLASS__, 'Vision2_func'));
 
 		add_action('init', array(__CLASS__, 'register_script'));
 		add_action('wp_footer', array(__CLASS__, 'print_script'));
 	}
 
-	static function handle_v2_shortcode($atts) {
-		self::$add_v2_script = true;
+	static function handle_shortcode($atts) {
+		self::$add_script = true;
         //Start Shorcode Handling
-            add_v2_shortcode( 'Vision2', 'Vision2_func' );
             //[Vision2 module="MODULE HERE" opts="OPTIONS HERE"]
             function Vision2_func( $atts ) 
             {
@@ -39,13 +38,13 @@ class Vision2_Shortcode {
 
 	static function register_script() {
     //Insert Script, If Function Present
-		wp_register_script('V2-script', '//cflcn.v2sapi.co/script/APIScript?includeDemoCss=true');
+		wp_register_script('Vision2-script', '//cflcn.v2sapi.co/script/APIScript?includeDemoCss=true');
 	}
 
 	static function print_script() {
-		if ( ! self::$add_v2_script )
+		if ( ! self::$add_script )
 			return;
 
-		wp_print_scripts('V2-script');
+		wp_print_scripts('Vision2-script');
 	}
 }
