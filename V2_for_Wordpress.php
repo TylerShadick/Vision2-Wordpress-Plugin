b51c9148b82ec34ab7c60274c6205f4032b8eb7a
Tyler Shadick <tyler.shadick@gmail.com>
2015-10-23 13:40:24 -0400

    Try overriding Enqueue

--- V2_for_Wordpress.php
+++ V2_for_Wordpress.php
@@ -14,13 +14,11 @@ include( plugin_dir_path( __FILE__ ) . 'V2_Options.php');
 class Vision2{
 	static $add_script;
     static $add_siteid;
-
 	static function init() {
 		add_shortcode('Vision2', array(__CLASS__, 'handle_shortcode'));
 		add_action('init', array(__CLASS__, 'register_script'));
 		add_action('wp_footer', array(__CLASS__, 'print_script'));
 	}
-
 	static function handle_shortcode($atts) {
 		self::$add_script = true;
                 $a = shortcode_atts( array(
@@ -30,7 +28,7 @@ class Vision2{
                 ), $atts );
         if(isset($a['siteid']) && ctype_alnum($a['siteid']))
         {
-            self::$add_siteid = '&V2wsid=' . $a['siteid'];
+            self::$add_siteid = '&v2wsid='$a['siteid'];
         }
         if(isset($a['opts']))
         {
@@ -50,15 +48,21 @@ class Vision2{
          $V2Plugin_plugin_options = get_option('V2Plugin_plugin_options');
     if(isset($V2Plugin_plugin_options['V2Plugin_API_URL']))
        {
-    wp_register_script('vision2_api', 'https://'. $V2Plugin_plugin_options['V2Plugin_API_URL'] .'.v2sapi.co/script/APIScript?includeDemoCss=true' . self::$add_siteid);	}
+    wp_register_script('vision2_api', 'https://'. $V2Plugin_plugin_options['V2Plugin_API_URL'] .'.v2sapi.co/script/APIScript?includeDemoCss=true');	}
        }
-
 	static function print_script() {
         $V2Plugin_plugin_options = get_option('V2Plugin_plugin_options');
 		if ( ! self::$add_script && isset($V2Plugin_plugin_options['V2Plugin_API_URL']) )
 			return;
         //Fires only is shortcode is present
-		wp_enqueue_script('vision2_api');
+        if (isset(self::$add_siteid))
+        {
+		wp_enqueue_script('vision2_api', 'https://'. $V2Plugin_plugin_options['V2Plugin_API_URL'] .'.v2sapi.co/script/APIScript?includeDemoCss=true' . self::$add_siteid);
+        }
+        else
+        {
+            wp_enqueue_script('vision2_api', 'https://'. $V2Plugin_plugin_options['V2Plugin_API_URL'] .'.v2sapi.co/script/APIScript?includeDemoCss=true');
+        }
 	}
 }
 Vision2::init();
